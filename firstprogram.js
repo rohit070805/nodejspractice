e13b16f504e123104220467c29e7410ddd28ef75
Rohit Kumar <rohitdhankhar7347@gmail.com>
2025-06-04 17:24:59 +0530

    Added first program to Run console.log

---
+++ firstprogram.js
@@ -0,0 +1,6 @@
+console.log("Hello");
+var x = 10;
+var y = 20;
+console.warn(x+y);
+// node js me console.log/console.warn/console.error 
+// inks use debugging me hi hota h bs ki kb kya fn or konsi condition chl rhi
