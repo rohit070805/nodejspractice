f92ce1adf1f22c5afb8b5e7bdab376117fa57306
Rohit Kumar <rohitdhankhar7347@gmail.com>
2025-06-04 17:27:57 +0530

    Added first program to Run console.log

--- firstprogram.js
+++ firstprogram.js
@@ -4,3 +4,7 @@ var y = 20;
 console.warn(x+y);
 // node js me console.log/console.warn/console.error 
 // inks use debugging me hi hota h bs ki kb kya fn or konsi condition chl rhi
+
+/* Interview Question
+ye console.log jo browser me console.log hota h us se alag hai
+ye nodejs ka apna console.log hai*/
